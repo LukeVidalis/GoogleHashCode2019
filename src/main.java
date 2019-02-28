6333311c5d3f9fc7aeb3f72e9edf4bb67ecf6a17
LukeVidalis <loukaslks@gmail.com>
2019-02-28 19:32:30 +0000

    Started setting up the main class -Split photos into vertical and horizontal Arrays

--- src/main.java
+++ src/main.java
@@ -1,8 +1,41 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class main {
+	private ArrayList<Slide> slides;
+	private ArrayList<Photo> verticalPhotos;
+	private ArrayList<Photo> horizontalPhotos;
 
-	public static void main(String[] args) {
-		
+	private ArrayList<String> data;
+
+	private fileReader fr;
+
+	public main() throws IOException {
+		slides = new ArrayList<Slide>();
+		verticalPhotos = new ArrayList<Photo>();
+
+		fr = new fileReader(0);
+		data = fr.getFileData();
+		setupPhotos(data);
+	}
+
+	private void setupPhotos(ArrayList<String> list) {
+		Photo p;
+		for (String line : list) {
+			p = new Photo(line);
+			if(p.getOrientation().equals("V")) {
+				verticalPhotos.add(p);
+			}else if(p.getOrientation().equals("H")) {
+				horizontalPhotos.add(p);
+			}else {
+				System.out.println("Error: Wrong orientation");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		new main();
 	}
 
 }
