914a9887a3b338e28d5fa02cb08dd074f3e0515f
LukeVidalis <loukaslks@gmail.com>
2019-02-28 18:26:41 +0000

    Implemented method for finding the common tags of two photos

--- src/Slide.java
+++ src/Slide.java
@@ -3,28 +3,33 @@ import java.util.ArrayList;
 public class Slide {
 	private ArrayList<Photo> photos = new ArrayList<Photo>();
 	private ArrayList<String> tags = new ArrayList<String>();
-	
 
 	public Slide(ArrayList<Photo> photos) {
 		this.photos = photos;
 		setupTags();
 	}
-	
+
 	private void setupTags() {
-		if(photos.size()==1) {
-			
+		if (photos.size() == 1) {
+			tags = photos.get(0).getTags();
+		} else {
+			ArrayList<String> tagsA= photos.get(0).getTags();
+			ArrayList<String> tagsB= photos.get(1).getTags();
+			ArrayList<String> commonTags = new ArrayList<String>(tagsA);
+			commonTags.retainAll(tagsB);
+			tags=commonTags;
 		}
 	}
 
 	public String toString() {
-		String s="";
-		for(int i=0;i<photos.size();i++) {
-			if(i>0) {
-				s=s+" ";
+		String s = "";
+		for (int i = 0; i < photos.size(); i++) {
+			if (i > 0) {
+				s = s + " ";
 			}
-			s=s+photos.get(i).getPhotoID();
+			s = s + photos.get(i).getPhotoID();
 		}
 		return s;
 	}
-	
+
 }
