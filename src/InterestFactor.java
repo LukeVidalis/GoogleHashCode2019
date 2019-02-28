7506c7602b24727d26ff1b8ac5e15066f0e4893c
LukeVidalis <loukaslks@gmail.com>
2019-02-28 18:57:52 +0000

    Implemented Interest factor value

--- src/InterestFactor.java
+++ src/InterestFactor.java
@@ -5,24 +5,52 @@ public class InterestFactor {
 	private Slide b;
 	private int commonTags;
 
-
 	private int AnotBtags;
 	private int BnotAtags;
 
 	public InterestFactor(Slide a, Slide b) {
-		this.a=a;
-		this.b=b;
+		this.a = a;
+		this.b = b;
 		calcInterest();
 	}
 
 	private void calcInterest() {
-		ArrayList<String> tagsA= a.getTags();
-		ArrayList<String> tagsB= b.getTags();
+		ArrayList<String> tagsA = a.getTags();
+		ArrayList<String> tagsB = b.getTags();
 		ArrayList<String> commonTagsArr = new ArrayList<String>(tagsA);
 		commonTagsArr.retainAll(tagsB);
-		commonTags=commonTagsArr.size();
+		commonTags = commonTagsArr.size();
+		AnotBtags = retainUniqueNumber(tagsA, tagsB);
+		BnotAtags = retainUniqueNumber(tagsB, tagsA);
+
 	}
-	
+
+	public Integer retainUniqueNumber(ArrayList<String> listA, ArrayList<String> listB) {
+
+		ArrayList<String> newList = new ArrayList<String>();
+
+		// Traverse through the first list
+		for (String element : listA) {
+
+			if (!listB.contains(element)) {
+
+				newList.add(element);
+			}
+		}
+
+		// return the new list
+		return newList.size();
+	}
+
+	public Integer getInterestFactor() {
+		ArrayList<Integer> valueFactors = new ArrayList<Integer>();
+		valueFactors.add(commonTags);
+		valueFactors.add(AnotBtags);
+		valueFactors.add(BnotAtags);
+		valueFactors.sort(null);
+		return valueFactors.get(0);
+	}
+
 	public Slide getA() {
 		return a;
 	}
